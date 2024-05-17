db48ab1daf47933dedd600839661fe5537307432
Hector <hectorvallsmira2003@gmail.com>
2024-05-17 12:34:10 +0200

    Image hechas

--- migrator_indexeo.php
+++ migrator_indexeo.php
@@ -322,6 +322,13 @@ class Migrator_indexeo extends Module
                 $this->populateCMSShop($conn,$prefix);
 
 
+                //  IMAGE------------------------------------------------------------------------
+                $this->populateImage($conn,$prefix);
+                $this->populateImageLang($conn,$prefix);
+                $this->populateImageShop($conn,$prefix);
+                $this->populateImageType($conn,$prefix);
+
+
 
             }
             catch(PDOException $exception) {
@@ -3642,6 +3649,133 @@ class Migrator_indexeo extends Module
     }
 
 
+
+
+    public function populateImage($conn, $prefix){
+        try {
+            $query = $conn->prepare("SELECT * FROM " .$prefix. "image WHERE 1");
+            $query->execute();
+
+            Db::getInstance()->execute("DELETE FROM ". _DB_PREFIX_ ."image WHERE 1;");
+            foreach($query->fetchAll() as $key=>$value) {
+                $sql = "INSERT INTO " . _DB_PREFIX_ . "image (
+                    `id_image`, 
+                    `id_product`,
+                    `position`,
+                    `cover`
+                ) 
+                VALUES (
+                    '" . pSQL($value['id_image']) . "', 
+                    '" . pSQL($value['id_product']) . "',
+                    '" . pSQL($value['position']) . "',
+                    NULL
+                )";
+                Db::getInstance()->execute($sql);
+            }
+        }
+        catch(PDOException $exception) {
+            echo "Error: " . $exception->getMessage();
+        }
+        // Cerrar conexion
+        $conn = null;
+    }
+
+
+    public function populateImageLang($conn, $prefix){
+        try {
+            $query = $conn->prepare("SELECT * FROM " .$prefix. "image_lang WHERE 1");
+            $query->execute();
+
+            Db::getInstance()->execute("DELETE FROM ". _DB_PREFIX_ ."image_lang WHERE 1;");
+            foreach($query->fetchAll() as $key=>$value) {
+                $sql = "INSERT INTO " . _DB_PREFIX_ . "image_lang (
+                    `id_image`, 
+                    `id_lang`,
+                    `legend`
+                ) 
+                VALUES (
+                    '" . pSQL($value['id_image']) . "', 
+                    '" . pSQL($value['id_lang']) . "',
+                    '" . pSQL($value['legend']) . "'
+                )";
+                Db::getInstance()->execute($sql);
+            }
+        }
+        catch(PDOException $exception) {
+            echo "Error: " . $exception->getMessage();
+        }
+        // Cerrar conexion
+        $conn = null;
+    }
+
+    public function populateImageShop($conn, $prefix){
+        try {
+            $query = $conn->prepare("SELECT * FROM " .$prefix. "image_shop WHERE 1");
+            $query->execute();
+
+            Db::getInstance()->execute("DELETE FROM ". _DB_PREFIX_ ."image_shop WHERE 1;");
+            foreach($query->fetchAll() as $key=>$value) {
+                $sql = "INSERT INTO " . _DB_PREFIX_ . "image_shop (
+                    `id_image`, 
+                    `id_product`,
+                    `id_shop`,
+                    `cover`
+                ) 
+                VALUES (
+                    '" . pSQL($value['id_image']) . "', 
+                    '" . pSQL($value['id_product']) . "',
+                    '" . pSQL($value['id_shop']) . "',
+                    NULL
+                )";
+                Db::getInstance()->execute($sql);
+            }
+        }
+        catch(PDOException $exception) {
+            echo "Error: " . $exception->getMessage();
+        }
+        // Cerrar conexion
+        $conn = null;
+    }
+
+
+    public function populateImageType($conn, $prefix){
+        try {
+            $query = $conn->prepare("SELECT * FROM " .$prefix. "image_type WHERE 1");
+            $query->execute();
+
+            Db::getInstance()->execute("DELETE FROM ". _DB_PREFIX_ ."image_type WHERE 1;");
+            foreach($query->fetchAll() as $key=>$value) {
+                $sql = "INSERT INTO " . _DB_PREFIX_ . "image_type (
+                    `id_image_type`, 
+                    `name`,
+                    `width`,
+                    `height`,
+                    `products`,
+                    `categories`,
+                    `suppliers`,
+                    `stores`,
+                    `manufacturers`
+                ) 
+                VALUES (
+                    '" . pSQL($value['id_image_type']) . "', 
+                    '" . pSQL($value['name']) . "',
+                    '" . pSQL($value['width']) . "',
+                    '" . pSQL($value['height']) . "',
+                    '" . pSQL($value['products']) . "',
+                    '" . pSQL($value['categories']) . "',
+                    '" . pSQL($value['suppliers']) . "',
+                    '" . pSQL($value['stores']) . "',
+                    '" . pSQL($value['manufacturers']) . "'
+                )";
+                Db::getInstance()->execute($sql);
+            }
+        }
+        catch(PDOException $exception) {
+            echo "Error: " . $exception->getMessage();
+        }
+        // Cerrar conexion
+        $conn = null;
+    }
 
 
 
